4ecf1f8e278ce04af87de481ed0a70bc973aeb1a
Felpu01 <matiasgonzalezleonel@icloud.com>
2026-05-20 15:17:30 -0300

    Create routes.py

---
+++ api/routes.py
@@ -0,0 +1,22 @@
+from flask import Blueprint, request, jsonify
+from core.brain import brain
+
+api = Blueprint("api", __name__)
+
+@api.route("/chat", methods=["POST"])
+def chat():
+    data = request.json
+    message = data.get("message", "")
+
+    response = brain(message)
+
+    return jsonify({"response": response})
+
+@api.route("/voice", methods=["POST"])
+def voice():
+    data = request.json
+    text = data.get("text", "")
+
+    response = brain(text)
+
+    return jsonify({"response": response})
