754da084a90ae0c09cda35619f5298a78d078c0b
Felpu01 <matiasgonzalezleonel@icloud.com>
2026-05-20 15:45:53 -0300

    Update brain.py

--- core/brain.py
+++ core/brain.py
@@ -1,10 +1,19 @@
-from core.router import route_intent
-from memory.memory import save_memory, get_memory
+from memory.memory import save_memory
+from memory.intelligence import classify_message
+from core.decision_engine import decide, execute_decision
 
 def brain(message):
 
+    # 1. guardar memoria base
     save_memory("history", message)
 
-    response = route_intent(message)
+    # 2. clasificar intención
+    category = classify_message(message)
 
-    return response
+    # 3. decidir qué hacer
+    decision = decide(message, category)
+
+    # 4. ejecutar acción
+    result = execute_decision(decision, message)
+
+    return result
